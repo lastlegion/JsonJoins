e12ec816f9704f1e579fc93617b54a6644421803
Ganesh Iyer <lastlegion@gmail.com>
2015-07-29 23:37:02 +0530

    passing simple test

--- src/join.js
+++ src/join.js
@@ -1,6 +1,6 @@
+var extend = require("extend")
 
-
-var _mergetwo = function(data1, data2){
+var _mergetwo = function(data1, data2, keys){
     var merged = [];
     for(var i=0; i<data1.length; i++){
         var row1 = data1[i];
@@ -18,15 +18,15 @@ var _mergetwo = function(data1, data2){
 };
 
 
-var _merge = function(sources){
+var _merge = function(sources, keys){
         if(sources.length == 1){
             return sources;
         } else if(sources.length == 2){
-            return _mergetwo(sources[0],sources[1])
+            return _mergetwo(sources[0],sources[1], keys)
         } else {
 
             //merge first 2
-            var m2 = _mergetwo(sources[0], sources[1])
+            var m2 = _mergetwo(sources[0], sources[1], keys)
             sources.shift(2);
             sources = sources.push(m2);
             _merge(sources)
